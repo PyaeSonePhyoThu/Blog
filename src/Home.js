35ed9ffa5c657fc1ab92d3b1ce92c01c1fc875a4
pyaesone0193 <108206395+pyaesone0193@users.noreply.github.com>
2024-02-25 16:35:31 +0630

    hello

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,24 @@
+import {useState} from 'react';
+
 const Home = () => {
+
+    const [blogs, setBlogs] = useState(
+    [
+        {title : 'My new Website', body : 'Lorem', author : 'Mario', id : 1},
+        {title : 'My new Website', body : 'Lorem', author : 'Luigi', id : 2},
+        {title : 'My new Website', body : 'Lorem', author : 'May', id : 3},
+    ]);
     return (  
         <div className="home">
-            <h2>Home Page</h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key = {blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
  
 export default Home;
+
